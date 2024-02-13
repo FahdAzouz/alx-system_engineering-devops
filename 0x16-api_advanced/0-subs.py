ebb4989849cf0af99e2a1c0b6048ec24989db32c
FahdAzouz <fahdstudies00@gmail.com>
2024-02-13 20:10:20 +0100

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Function to quuery subscribers on a given Reddit subreddit"""
 import requests
 
 
@@ -12,5 +12,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    body = response.json().get('data')
+    return body.get('subscribers')
