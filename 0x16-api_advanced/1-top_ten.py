24e48feaadd23253f81af078a5dbe82c09b54ba1
FahdAzouz <fahdstudies00@gmail.com>
2024-02-13 20:38:28 +0100

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,8 @@ def top_ten(subreddit):
     """Return the top ten posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)"
     }
     params = {
         "limit": 10
